fbd6cf5335b58bef18e802d1d88a85018fcd4aec
pappacorleone <kmondlane@gmail.com>
2022-12-20 21:57:22 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,6 +8,8 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
+
+
 # Define the states
 class State:
     DELETE_TASK = 1
@@ -16,7 +18,8 @@ class State:
 states = {}
 
 def start(update, context):
-    update.message.reply_text('Welcome to the Task Manager chatbot! Use /menu to see the available commands.')
+    update.message.
+    _text('Welcome to the Task Manager chatbot! Use /menu to see the available commands.')
 
 def handle_command(update):
     # Split the command into the command and arguments
@@ -46,14 +49,61 @@ def handle_command(update):
         task = ' '.join(args)
         add_task(update.message.chat_id, task, frequency=frequency)
         update.message.reply_text('Task added!')
-    elif command == '/delete':
-        # Get the list of tasks
-        tasks = get_tasks(update.message.chat_id)
-        # Display the tasks as a list of options
-        options = '\n'.join(f'{i+1}. {task[0]}' for i, task in enumerate(tasks))
-        message = 'Please select a task to delete:\n' + options
-        # Send the message as a reply keyboard
-        reply_keyboard = [[str(i+1)] for i in range(len(tasks))]
-        update.message.reply_text(
-            message,
-            reply_markup=ReplyKeyboardMarkup(
+        
+def delete(update, context):
+    # Get the list of tasks
+    tasks = get_tasks(update.message.chat_id)
+    # Display the tasks as a list of options
+    options = '\n'.join(f'{i+1}. {task[0]}' for i, task in enumerate(tasks))
+    message = 'Please select a task to delete:\n' + options
+    # Send the message as a reply keyboard
+    reply_keyboard = [[str(i+1)] for i in range(len(tasks))]
+    update.message.reply_text(
+        message,
+        reply_markup=ReplyKeyboardMarkup(
+            reply_keyboard,
+            one_time_keyboard=True,
+            selective=True,
+        )
+    )
+    # Store the state in the dictionary
+    states[update.message.chat_id] = State.DELETE_TASK
+
+def handle_response(update, context):
+    # Get the state from the dictionary
+    state = states.get(update.message.chat_id)
+    if state == State.DELETE_TASK:
+        # Get the index of the task to delete
+        try:
+            index = int(update.message.text) - 1
+            # Delete the task
+            delete_task(update.message.chat_id, index)
+            update.message.reply_text('Task deleted!')
+        except ValueError:
+            update.message.reply_text('Invalid option. Please try again.')
+        except IndexError:
+            update.message.reply_text('Task not found. Please try again.')
+        # Remove the state from the dictionary
+        states.pop(update.message.chat_id, None)
+
+# Create the Updater and pass it the bot's token
+updater = Updater(TOKEN, use_context=True)
+
+# Get the dispatcher to register handlers
+dp = updater.dispatcher
+
+# Add the start command handler
+dp.add_handler(CommandHandler('start', start))
+
+# Add the delete command handler
+dp.add_handler(CommandHandler('delete', delete))
+
+# Add the message handler for processing responses
+dp.add_handler(MessageHandler(Filters.text, handle_response))
+
+# Start the bot
+updater.start_polling()
+
+# Run the bot until the user presses Ctrl-C or the process receives SIGINT,
+# SIGTERM or SIGABRT
+updater.idle()
